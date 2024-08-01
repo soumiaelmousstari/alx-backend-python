74a39d88a4ac40e91779d27d4e814de00aa91adc
soumiaelmousstari <elmousstarisoumia@gmail.com>
2024-08-01 20:30:18 +0000

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """1. Basic annotations - concat"""
 
+
 def concat(str1: str, str2: str) -> str:
     """function concat that takes a string str1
     and a string str2 as arguments and returns a
