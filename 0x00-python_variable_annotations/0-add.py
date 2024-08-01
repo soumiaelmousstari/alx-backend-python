e410018f05431ebc7283d3d3947e112d13f0978c
soumiaelmousstari <elmousstarisoumia@gmail.com>
2024-08-01 20:12:53 +0000

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""0. Basic annotations - add"""
+def add(a, b)
+    """ function add that takes a float a and a float
+    b as arguments and returns their sum as a float."""
+    return a + b
