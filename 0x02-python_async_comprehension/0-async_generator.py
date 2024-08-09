6974c0da09df009af8b29b7dab9928266e0a13e0
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-08-09 17:04:39 +0000

    0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Task 0"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """a coroutine called async_generator that takes no arguments"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
