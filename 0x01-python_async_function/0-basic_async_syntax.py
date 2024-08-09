daa34a63316ea1f4753188f5872fa844b63ce332
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-08-09 16:45:22 +0000

    .

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,10 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """an asynchronous coroutine that takes in an integer argument (max_delay, with a default value of 10) named wait_random that waits for a random delay between 0 and max_delay (included and float value) seconds and eventually returns it."""
+    """an asynchronous coroutine that takes in an integer argument
+    (max_delay, with a default value of 10) named wait_random
+    that waits for a random delay between 0 and
+    max_delay (included and float value) seconds and eventually returns it."""
     wait = random.random() * max_delay
     await asyncio.sleep(wait)
     return wait
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay."""
+    """ and write an async routine called wait_n that takes in 2
+    int arguments (in this order): n and max_delay."""
     wait = await asyncio.gather(
         *tuple(map(lambda _: wait_random(max_delay), range(n)))
     )
