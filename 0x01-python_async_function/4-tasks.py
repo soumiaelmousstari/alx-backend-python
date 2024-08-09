651d92e9fa1e74aa7975bf60e7a00abeb121d40c
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-08-09 16:58:12 +0000

    2. Measure the runtime

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-'''Task 4's module.
-'''
+"""Task 4"""
 import asyncio
 from typing import List
 
@@ -9,9 +8,10 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes task_wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
+    """Take the code from wait_n and alter it into a new function
+    task_wait_n. The code is nearly identical to wait_n except
+    task_wait_random is being called."""
+    wait = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
-    return sorted(wait_times)
+    return sorted(wait)
